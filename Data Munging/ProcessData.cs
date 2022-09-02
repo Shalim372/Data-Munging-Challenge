af5d04e5442216985c779034539923f926ea87f5
shali <shali@DESKTOP-3EC031V>
2022-09-02 08:53:11 -0500

    Code refractor and common method

---
+++ Data Munging/ProcessData.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Munging
+{
+    public class ProcessData
+    {
+        public static string FindLowestData(string filePath, int firstColumn, int secondColumn, int returnColumn)
+        {
+            string finalValue = "";
+            Double min = Double.MaxValue, col1, col2, col3, diff;
+
+            try
+            {
+                IEnumerable<string> text = File.ReadAllLines(filePath);
+                foreach (string line in text)
+                {
+                    string[] eachLine = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                    if (eachLine.Length > 0 && Double.TryParse(eachLine[0].Replace(".", ""), out col1))
+                    {
+                        col2 = Convert.ToInt32(eachLine[firstColumn].Replace("*", ""));
+                        col3 = Convert.ToInt32(eachLine[secondColumn].Replace("*", ""));
+                        diff = Math.Abs(col2 - col3);
+
+                        if (diff <= min)
+                        {
+                            min = diff;
+                            finalValue = eachLine[returnColumn];
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error processing data");
+            }
+
+            return finalValue;
+        }
+    }
+}
--- Data Munging/Program.cs
+++ Data Munging/Program.cs
@@ -2,9 +2,15 @@
 using Data_Munging;
 
 //1. Weather Data
-Weather weather = new Weather();
-Console.WriteLine("1. Day with smallest temperature spread : "+weather.WeatherDayMin());
+//Weather weather = new Weather();
+//Console.WriteLine("1. Day with smallest temperature spread : "+weather.WeatherDayMin());
 
 //2. Football Data
-Soccer soccer = new Soccer();
-Console.WriteLine("2. Team with the smallest difference in goals : " + soccer.TeamWithSmallDiff());
+//Soccer soccer = new Soccer();
+//Console.WriteLine("2. Team with the smallest difference in goals : " + soccer.TeamWithSmallDiff());
+
+//3. Dry Fusion
+string path = Path.GetFullPath(@"S:\New folder\");
+
+Console.WriteLine("Day: " + ProcessData.FindLowestData(path + "weather.dat", 1, 2, 0));
+Console.WriteLine("Team: " + ProcessData.FindLowestData(path + "football.dat", 6, 8, 1));
